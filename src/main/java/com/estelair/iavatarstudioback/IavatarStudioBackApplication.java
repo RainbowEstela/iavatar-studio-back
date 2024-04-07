7db3c955dccb2bccd47aed6041e55159e13dcdea
Estela <eloiseruiz15@gmail.com>
2024-04-07 09:53:32 +0200

    primer commit

---
+++ src/main/java/com/estelair/iavatarstudioback/IavatarStudioBackApplication.java
@@ -0,0 +1,13 @@
+package com.estelair.iavatarstudioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IavatarStudioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IavatarStudioBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estelair/iavatarstudioback/IavatarStudioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estelair.iavatarstudioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IavatarStudioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
