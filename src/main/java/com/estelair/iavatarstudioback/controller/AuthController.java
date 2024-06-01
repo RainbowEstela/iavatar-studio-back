1694a30c3843803a694d2c8e60a0ba378813428f
Estela <eloiseruiz15@gmail.com>
2024-06-01 16:58:05 +0200

    cors para controller login register

--- src/main/java/com/estelair/iavatarstudioback/controller/AuthController.java
+++ src/main/java/com/estelair/iavatarstudioback/controller/AuthController.java
@@ -12,11 +12,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
